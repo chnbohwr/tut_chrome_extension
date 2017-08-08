368e8724e06d9b50e027fcc70dcea98f7d799b98
chnbohwr <chnbohwr@gmail.com>
2017-08-08 23:37:28 +0800

    move execute file to content inject and add some new function

--- execute.js
+++ execute.js
@@ -1,22 +1,99 @@
-if (!extensionLineComments) {
-  var extensionLineComments = Array.from(document.querySelector('.comment_list').children);
-  extensionLineComments = extensionLineComments.filter((item, index) => {
-    var userLink = item.querySelector('.nickname a').getAttribute('href');
-    return extensionLineComments.findIndex((item2) => {
-      return userLink === item2.querySelector('.nickname a').getAttribute('href');
-    }) === index;
+let extensionLineTags = 0;
+let extensionLineKeyword = '';
+let extensionLineIndexMap = {};
+let extensionLineComments = [];
+
+const filterComment = (comments) => {
+  const filtedComment = comments.filter((item) => {
+    const tags = item.querySelectorAll('a[classname="_tl_profile_nickname"]');
+    console.log(extensionLineTags)
+    if (tags.length < extensionLineTags) {
+      return false;
+    }
+    const keywordIndex = item.querySelector('.comment').textContent.indexOf(extensionLineKeyword);
+    if (keywordIndex === -1) {
+      return false;
+    }
+    return true;
   });
-  var extensionLineIndexMap = {};
+
+  const filtedMap = filtedComment.reduce((acc, item) => {
+    const userLink = item.querySelector('.nickname a').getAttribute('href');
+    if (!acc[userLink]) {
+      acc[userLink] = item;
+    }
+    return acc;
+  }, {});
+
+  return Object.keys(filtedMap).map(key => filtedMap[key]);
+};
+
+const clickEvent = () => {
+  if (!extensionLineComments.length) {
+    extensionLineComments = filterComment(Array.from(document.querySelector('.comment_list').children));
+    extensionLineIndexMap = {};
+  }
+
+  if (Object.keys(extensionLineIndexMap).length === extensionLineComments.length) {
+    alert('沒有可以抽選的對象');
+  } else {
+    let randomIndex = 0;
+    do {
+      randomIndex = Math.floor(Math.random() * extensionLineComments.length);
+    } while (extensionLineIndexMap[randomIndex] === true)
+    extensionLineIndexMap[randomIndex] = true;
+    const comment = extensionLineComments[randomIndex];
+    comment.style.background = 'yellow';
+    comment.scrollIntoView();
+    window.scrollTo(0, window.scrollY - 200);
+  }
+};
+
+const setTags = (value) => {
+  console.log('set tag', value);
+  extensionLineTags = value;
+};
+
+const setKeyword = (value) => {
+  extensionLineKeyword = value;
+};
+
+const reset = () => {
+  extensionLineIndexMap = {};
+  extensionLineComments = [];
+  Array
+    .from(document.querySelector('.comment_list').children)
+    .forEach((item) => { item.style.background = 'none'; });
+};
+
+const onMessage = (message, sender, response) => {
+  switch (message.action) {
+    case 'CLICK':
+      clickEvent();
+      break;
+    case 'RESET':
+      reset();
+      break;
+    case 'SET_TAGS':
+      setTags(message.value);
+      break;
+    case 'SET_KEYWORD':
+      setKeyword(message.value);
+      break;
+    default:
+      break;
+  }
 }
-if (Object.keys(extensionLineIndexMap).length === extensionLineComments.length) {
-  alert('沒有可以抽選的對象');
-} else {
-  do {
-    var randomIndex = Math.floor(Math.random() * extensionLineComments.length);
-  } while (extensionLineIndexMap[randomIndex] === true)
-  extensionLineIndexMap[randomIndex] = true;
-  var comment = extensionLineComments[randomIndex];
-  comment.style.background = 'yellow';
-  comment.scrollIntoView();
-  window.scrollTo(0, window.scrollY - 200);
-}
+
+const clickMore = () => {
+  const button = document.querySelector('.article_comment a[href="#more"]');
+  if(button){
+    button.click();
+    setTimeout(clickMore, 200);
+  }
+};
+
+clickMore();
+
+chrome.runtime.onMessage.addListener(onMessage);
+
--- main.js
+++ main.js
@@ -1,5 +1,49 @@
-$('#checkPage').on('click', ()=>{
-  chrome.tabs.executeScript({
-    file: './execute.js',
+
+var getSelectedTab = (tab) => {
+
+  var tabId = tab.id;
+  var sendMessage = (messageObj) => {
+    chrome.tabs.sendMessage(tabId, messageObj);
+  };
+
+  $('#lottery').on('click', () => {
+    $('#tags').attr('disabled', true);
+    $('#keyword').attr('disabled', true);
+    sendMessage({
+      action: 'CLICK'
+    });
   });
-});
+
+  $('#reset').on('click', () => {
+    $('#tags').attr('disabled', false);
+    $('#keyword').attr('disabled', false);
+    sendMessage({
+      action: 'RESET'
+    });
+  });
+
+  $('#tags').on('change', (e) => {
+    sendMessage({
+      action: 'SET_TAGS',
+      value: e.target.value
+    });
+  });
+
+  $('#keyword').on('change', (e) => {
+    sendMessage({
+      action: 'SET_KEYWORD',
+      value: e.target.value
+    });
+  });
+
+  sendMessage({
+    action: 'RESET'
+  });
+
+}
+
+chrome.tabs.getSelected(null, getSelectedTab);
+
+
+
+
